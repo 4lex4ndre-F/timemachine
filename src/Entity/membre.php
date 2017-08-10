a21f73da882c6ea1568ee84e1354eefc4dc6771d
Alexandre FERNANDES <4lex.fernandes@gmail.com>
2017-08-10 14:20:16 +0200

    création Entity/membre.php

---
+++ src/Entity/membre.php
@@ -0,0 +1,135 @@
+<?php
+
+
+
+namespace Entity;
+
+/**
+ * Description of membre
+ * variables + getters/setters
+ *
+ * @author Etudiant
+ */
+class membre {
+    /**
+     *
+     * @var int 
+     */
+    private $id;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $firstname;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $lastname;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $gender;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $pseudo;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $email;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $password;
+    
+    /**
+     *
+     * @var string 
+     */
+    private $status;
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getFirstname() {
+        return $this->firstname;
+    }
+
+    public function getLastname() {
+        return $this->lastname;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getPseudo() {
+        return $this->pseudo;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function getPassword() {
+        return $this->password;
+    }
+
+    public function getStatus() {
+        return $this->status;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setFirstname($firstname) {
+        $this->firstname = $firstname;
+        return $this;
+    }
+
+    public function setLastname($lastname) {
+        $this->lastname = $lastname;
+        return $this;
+    }
+
+    public function setGender($gender) {
+        $this->gender = $gender;
+        return $this;
+    }
+
+    public function setPseudo($pseudo) {
+        $this->pseudo = $pseudo;
+        return $this;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function setPassword($password) {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function setStatus($status) {
+        $this->status = $status;
+        return $this;
+    }
+
+
+}
