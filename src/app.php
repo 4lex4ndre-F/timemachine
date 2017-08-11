41954b84213a8aee6bf45ef399703a4eff9ca27c
Alexandre FERNANDES <4lex.fernandes@gmail.com>
2017-08-11 10:56:39 +0200

    initialisation de doctrine pour les requetes - installation : voir comm dans app.php

--- src/app.php
+++ src/app.php
@@ -2,9 +2,10 @@
 
 use Silex\Application;
 use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
@@ -16,5 +17,37 @@ $app['twig'] = $app->extend('twig', function ($twig, $app) {
 
     return $twig;
 });
+
+
+/**
+ * connexion à la BDD via :
+ * DOCTRINE ->
+ *  -> DBAL : couche de base doctrine ~PDO - Data Base Abstraction Layer
+ * 
+ * ===> Ajout doctrine DBAL ($app['db'] dans $app
+ * 
+ * nécéssite l'installation par composer:
+ *      composer require doctrine/dbal:~2.2
+ *      en ligne de commande dans le reperetoire de l'application
+ * 
+ * /!\ créer le controller
+ */
+$app->register(
+    new DoctrineServiceProvider(),
+    [
+        /* conventions de nomage */
+        'db.options' => [
+            'driver' => 'pdo_mysql',
+            'host' => 'localhost',
+            'dbname' => 'timemachine',
+            'user' => 'root',
+            'password' => '',
+            'charset' => 'utf8'
+        ]
+    ]
+);
+
+
+
 
 return $app;
